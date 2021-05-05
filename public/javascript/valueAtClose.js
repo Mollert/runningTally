4fd81e4d7417ecb66c4b4e768c49fbb96f766136
David <blueskies@carolina.rr.com>
2021-05-05 10:05:53 -0400

    Solved snapshot problem by removing .push

--- public/javascript/valueAtClose.js
+++ public/javascript/valueAtClose.js
@@ -3,16 +3,16 @@ let getUpdated = require("./readUpdated.js");
 let firstOfYear = require("./valueAtFirstOfYear.js");
 let theCommas = require("./addCommas.js");
 
-let updatedValues = getUpdated();
 
 let allCloseValues = [];
 
-let portTotal = firstOfYear.woFunds;
-let totalDifference = 0;
-let portPercentage = 0;
-
-
 const prepareCloseValue = (closes) => {
+
+	let updatedValues = getUpdated();
+
+	let portTotal = firstOfYear.woFunds;
+	let totalDifference = 0;
+	let portPercentage = 0;
 
 // Calculate close value of funds and add to portfolio value wo/fund value
 	for (i = 0 ; i < closes.length ; i++) {
@@ -21,15 +21,15 @@ const prepareCloseValue = (closes) => {
 
 		portTotal = portTotal + fundValue;
 	}
-	allCloseValues.push(theCommas(portTotal));
+	allCloseValues[0] = theCommas(portTotal);
 
 // Calculate if increase or decrease from first of year
 	if (portTotal > firstOfYear.total) {
-		allCloseValues.push("increase");
-		allCloseValues.push("black");
+		allCloseValues[1] = "increase";
+		allCloseValues[2] = "black";
 	} else {
-		allCloseValues.push("decrease");
-		allCloseValues.push("red");		
+		allCloseValues[1] = "decrease";
+		allCloseValues[2] = "red";
 	}
 
 // Calculate the difference from the first of the year
@@ -37,52 +37,54 @@ const prepareCloseValue = (closes) => {
 	if (totalDifference < 0) {
 		totalDifference = totalDifference * -1;
 	}
-	allCloseValues.push(theCommas(totalDifference));
+	allCloseValues[3] = theCommas(totalDifference);
 
 // Find percentage from first of year
 	portPercentage = (((portTotal - firstOfYear.total) / firstOfYear.total) * 100);
 	if (portPercentage < 0.1 && portPercentage > -0.1) {
-		allCloseValues.push("Almost 0");
+		allCloseValues[4] = "Almost 0";
 	} else {
-		allCloseValues.push(portPercentage.toFixed(1));
+		allCloseValues[4] = portPercentage.toFixed(1);		
 	}
 
+		let j = 5;
 // Calculate value and percentage of each fund
 	for (i = 0 ; i < closes.length ; i++) {
 // Claculate value of each fund
 		let currentValue = Number(closes[i]);
 		currentValue = (currentValue * firstOfYear.fundShares[i]);
 
-		allCloseValues.push(theCommas(currentValue));
+		allCloseValues[j] = theCommas(currentValue);		
 // Is current value of the current value a plus or minus
 		if (currentValue > (firstOfYear.fundValue[i] * firstOfYear.fundShares[i])) {
-			allCloseValues.push("black");
+			allCloseValues[j+1] = "black";
 		} else {
-			allCloseValues.push("red");				
+			allCloseValues[j+1] = "red";	
 		}
 
-		allCloseValues.push(((closes[i] - firstOfYear.fundValue[i]) / firstOfYear.fundValue[i] * 100).toFixed(1));	
+		allCloseValues[j+2] = ((closes[i] - firstOfYear.fundValue[i]) / firstOfYear.fundValue[i] * 100).toFixed(1);
 // Convert updated stirng of value to number and then see if close value is more or less of updated value
 		let updatedValueToNumber = updatedValues[i+6].replace(/,/g, "");
 		updatedValueToNumber = Number(updatedValueToNumber);
 		let sinceUpdateDifference = currentValue - updatedValueToNumber;
 		if (sinceUpdateDifference < 0) {
 			sinceUpdateDifference = sinceUpdateDifference * -1;
-			allCloseValues.push("decreased");
-			allCloseValues.push("red");			
+			allCloseValues[j+3] = "decrease";
+			allCloseValues[j+4] = "red";
 		} else {
-			allCloseValues.push("increased");
-			allCloseValues.push("black");			
+			allCloseValues[j+3] = "increase";
+			allCloseValues[j+4] = "black";						
 		}
 // What is the difference between updated value and closed value
-		allCloseValues.push(theCommas(sinceUpdateDifference));
+		allCloseValues[j+5] = theCommas(sinceUpdateDifference);		
 // What is the percentage difference between updated value and closed value
 		let fundPercentage = ((currentValue - updatedValueToNumber) / updatedValueToNumber * 100);
 		if (fundPercentage < 0.1 && fundPercentage > -0.1) {
-			allCloseValues.push("Almost 0");
+			allCloseValues[j+6] = "Almost 0";
 		} else {
-			allCloseValues.push(fundPercentage.toFixed(1));
+			allCloseValues[j+6] = fundPercentage.toFixed(1);			
 		}
+		j = j + 7;
 	}	
 
 	return allCloseValues
