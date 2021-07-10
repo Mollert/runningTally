b0ca1b4731d42dd831d271b54eb248e5424c2f00
David <blueskies@carolina.rr.com>
2021-07-10 19:04:12 -0400

    changed value wo funds

--- public/javascript/valueAtFirstOfYear.js
+++ public/javascript/valueAtFirstOfYear.js
@@ -1,7 +1,7 @@
 
 let firstOfYear = {
-	total: 1903231.00,
-	woFunds: 648239.51,
+	total: 1864231.00,
+	woFunds: 616385.80,
 	fundValue: [94.74, 181.18, 27.00, 178.36],
 	fundShares: [3313.16, 3146.76, 3409.09, 1537.00]
 }
